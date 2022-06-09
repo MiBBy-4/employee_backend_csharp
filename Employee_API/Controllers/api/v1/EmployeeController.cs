788df7629ae9bbe7d022f3f04c750556d3d0f69e
MiBBy-4 <75204033+MiBBy-4@users.noreply.github.com>
2022-06-09 23:02:06 +0300

    feat: implement SaveFiles method, that save Photos of employee in Photo folder

--- Employee_API/Controllers/api/v1/EmployeeController.cs
+++ Employee_API/Controllers/api/v1/EmployeeController.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using Employee_API.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 
 namespace Employee_API.Controllers.api.v1
@@ -12,11 +14,13 @@ namespace Employee_API.Controllers.api.v1
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        private readonly IConfiguration __configuration;
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
 
-        public EmployeeController(IConfiguration configuration)
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment env)
         {
-            __configuration = configuration;
+            _configuration = configuration;
+            _env = env;
         }
 
         [HttpGet]
@@ -24,7 +28,7 @@ namespace Employee_API.Controllers.api.v1
         {
             string getQuery = @"select employee_id, employee_name, convert(varchar(10), date_of_joining, 120) as date_of_joining, photo_file_name, department_id from Employee";
             DataTable table = new DataTable();
-            string sqlDataSource = __configuration.GetConnectionString("EmployeeAppCon");
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
             SqlDataReader sqlReader;
             using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
             {
@@ -45,7 +49,7 @@ namespace Employee_API.Controllers.api.v1
         {
             string getQuery = $@"insert into Employee values('{data.employee_name}', '{data.date_of_joining}','{data.photo_file_name}', '{data.depatrment_id}');";
             DataTable table = new DataTable();
-            string sqlDataSource = __configuration.GetConnectionString("EmployeeAppCon");
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
             SqlDataReader sqlReader;
             using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
             {
@@ -66,7 +70,7 @@ namespace Employee_API.Controllers.api.v1
         {
             string getQuery = $@"update Employee set employee_name = '{data.employee_name}', date_of_joining = '{data.date_of_joining}', photo_file_name = '{data.photo_file_name}', department_id = {data.depatrment_id} where employee_id = '{data.employee_id}'";
             DataTable table = new DataTable();
-            string sqlDataSource = __configuration.GetConnectionString("EmployeeAppCon");
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
             SqlDataReader sqlReader;
             using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
             {
@@ -87,7 +91,7 @@ namespace Employee_API.Controllers.api.v1
         {
             string getQuery = $@"delete from Employee where employee_id = {id}";
             DataTable table = new DataTable();
-            string sqlDataSource = __configuration.GetConnectionString("EmployeeAppCon");
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
             SqlDataReader sqlReader;
             using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
             {
@@ -108,7 +112,7 @@ namespace Employee_API.Controllers.api.v1
         {
             string getQuery = $@"select * from Employee where employee_id = '{id}'";
             DataTable table = new DataTable();
-            string sqlDataSource = __configuration.GetConnectionString("EmployeeAppCon");
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
             SqlDataReader sqlReader;
             using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
             {
@@ -122,6 +126,30 @@ namespace Employee_API.Controllers.api.v1
                 }
             }
             return new JsonResult(table);
+        }
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using(var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+                return new JsonResult("Something went wrong(");
+            }
         }
     }
 }
