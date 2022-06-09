09f270fac125278cf9422ca0bdf25d35fbf24962
MiBBy-4 <75204033+MiBBy-4@users.noreply.github.com>
2022-06-09 20:57:06 +0300

    feat: implement SWAGGER connection, also create Employee and Department models

---
+++ Employee_API/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace Employee_API.Models
+{
+    public class Department
+    {
+        public int department_id { get; set; }
+
+    }
+}
---
+++ Employee_API/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_API.Models
+{
+    public class Employee
+    {
+        public int employee_id { get; set; }
+        public string employee_name { get; set; }
+        public string date_of_joining { get; set; }
+        public string photo_file_name { get; set; }
+        public int depatrment_id { get; set; }
+    }
+}
