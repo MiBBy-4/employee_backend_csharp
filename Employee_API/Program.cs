71cfdf968bc5f0ca711875d9a179a741d0a52627
MiBBy-4 <75204033+MiBBy-4@users.noreply.github.com>
2022-06-11 22:39:58 +0300

    feat: set cors

--- Employee_API/Program.cs
+++ Employee_API/Program.cs
@@ -11,8 +11,16 @@ builder.Services.AddControllersWithViews().AddNewtonsoftJson(options => options.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(c => 
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod()
+     .AllowAnyHeader());
+});
+
 
 var app = builder.Build();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,11 +32,5 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
-    RequestPath = "/Photos"
-});
 
 app.Run();
